a594b91aed040ba388afafab777f9f8af0ce4b02
Kevin Ngo <k.ngo93@outlook.com>
2018-12-24 04:37:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  var int = parseInt(n, 10);
+  return int;
+}
+
+function preserveDecimal(n){
+  var num = parseFloat(n);
+  return num;
+}
